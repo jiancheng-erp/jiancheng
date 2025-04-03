9f644a3b889d3326922fe804df9a943833a779d7
as34htt <78578910+as34htt@users.noreply.github.com>
2025-04-03 16:31:59 +0800

    update revert instruction input
    同普通投产指令单填写更改

--- frontend/jiancheng/src/Pages/DevelopmentManager/views/RevertOrderDealView.vue
+++ frontend/jiancheng/src/Pages/DevelopmentManager/views/RevertOrderDealView.vue
@@ -928,13 +928,22 @@
                             <el-col :span="24" :offset="0">
                                 <el-table :data="getMaterialDataByType('hotsoleMaterialData')" border style="width: 100%">
                                     <el-table-column type="index"></el-table-column>
-                                    <el-table-column prop="materialType" label="材料类型" />
+                                    <el-table-column prop="materialType" label="材料类型">
+                                        <template #default="scope">
+                                            <el-select v-model="scope.row.materialType" :disabled="scope.row.materialType==='烫底'" filterable @change="handleMaterialTypeSelect(scope.row, $event)" @blur="trimInput(scope.row, 'materialType')">
+                                                <el-option v-for="item in materialTypeOptions" :key="item.value" :value="item.value" :label="item.label"> </el-option>
+                                            </el-select>
+                                        </template>
+                                        </el-table-column>
                                     <el-table-column prop="materialName" label="材料名称">
                                         <template #default="scope">
                                             <el-select v-model="scope.row.materialName" filterable @blur="trimInput(scope.row, 'materialName')" @change="handleMaterialNameSelect(scope.row, $event)">
                                                 <div v-if="scope.row.materialType === '里料'">
                                                     <el-option v-for="item in filterByTypes(materialNameOptions, [2])" :key="item.value" :value="item.value" :label="item.label"> </el-option>
                                                 </div>
+                                                <div v-else-if="scope.row.materialType === '面料'">
+                                                    <el-option v-for="item in filterByTypes(materialNameOptions, [1])" :key="item.value" :value="item.value" :label="item.label"> </el-option>
+                                                </div>                                                
                                                 <div v-else-if="scope.row.materialType === '烫底'">
                                                     <el-option v-for="item in filterByTypes(materialNameOptions, [16])" :key="item.value" :value="item.value" :label="item.label"> </el-option>
                                                 </div>
@@ -993,7 +1002,6 @@
                                                 v-model="scope.row.craftName"
                                                 size="default"
                                                 @blur="trimInput(scope.row, 'craftName')"
-                                                :disabled="scope.row.materialType === '烫底'"
                                                 :maxlength="wordLengths.CRAFT_NAME_LENGTH"
                                                 show-word-limit
                                             ></el-input>
@@ -1189,7 +1197,11 @@ export default {
                 SUPPLIER_NAME_LENGTH: constants.SUPPLIER_NAME_LENGTH
             },
             currentOrderShoeRow: {},
-            revertData: {}
+            revertData: {},
+            materialTypeOptions: [
+                { value: '面料', label: '面料' },
+                { value: '里料', label: '里料' },
+            ],
         }
     },
     async mounted() {
