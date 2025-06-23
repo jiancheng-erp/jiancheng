5203c4a8a8793355fb889fc11b219dc112fbef24
as34htt <78578910+as34htt@users.noreply.github.com>
2025-06-23 17:11:50 +0800

    update order confirm task auto refresh
    更新总经理确认订单、退回订单时列表自动刷新

--- frontend/jiancheng/src/Pages/CompanyManger/components/OrderConfirmation/OrderConfirmDetail.vue
+++ frontend/jiancheng/src/Pages/CompanyManger/components/OrderConfirmation/OrderConfirmDetail.vue
@@ -8,28 +8,18 @@
                 <el-row :gutter="0">
                     <el-col :span="24" :offset="0">
                         <el-descriptions title="" :column="3" border>
-                            <el-descriptions-item label="订单编号" align="center">{{
-                                orderData.orderRid
-                            }}</el-descriptions-item>
+                            <el-descriptions-item label="订单编号" align="center">{{ orderData.orderRid }}</el-descriptions-item>
                             <el-descriptions-item label="客户订单" align="center">
                                 {{ orderData.orderCid }}
                             </el-descriptions-item>
-                            <el-descriptions-item label="客户信息" align="center">{{
-                                orderData.customerInfo
-                            }}</el-descriptions-item>
-                            <el-descriptions-item label="订单周期" align="center">{{
-                                orderData.dateInfo
-                            }}</el-descriptions-item>
-                            <el-descriptions-item label="配码类型" align="center">{{
-                                orderData.batchInfoTypeName
-                            }}</el-descriptions-item>
+                            <el-descriptions-item label="客户信息" align="center">{{ orderData.customerInfo }}</el-descriptions-item>
+                            <el-descriptions-item label="订单周期" align="center">{{ orderData.dateInfo }}</el-descriptions-item>
+                            <el-descriptions-item label="配码类型" align="center">{{ orderData.batchInfoTypeName }}</el-descriptions-item>
                             <el-descriptions-item label="订单业务员" align="center">
                                 {{ orderData.orderStaffName }}
                             </el-descriptions-item>
                             <el-descriptions-item label="退回订单" align="center">
-                                <el-button type="danger " size="default" @click="openRevertDialog"
-                                    >退回订单</el-button
-                                >
+                                <el-button type="danger " size="default" @click="openRevertDialog">退回订单</el-button>
                             </el-descriptions-item>
                         </el-descriptions>
                     </el-col>
@@ -64,56 +54,29 @@
                             >
                                 <el-table-column type="expand">
                                     <template #default="scope">
-                                        <el-table
-                                            :data="scope.row.shoeTypeBatchInfoList"
-                                            :header-row-style="tableHeaderStyle"
-                                        >
+                                        <el-table :data="scope.row.shoeTypeBatchInfoList" :header-row-style="tableHeaderStyle">
                                             <el-table-column type="index"></el-table-column>
+                                            <el-table-column prop="packagingInfoName" label="配码名称" width="180" />
                                             <el-table-column
-                                                prop="packagingInfoName"
-                                                label="配码名称"
-                                                width="180"
-                                            />
-                                            <el-table-column
-                                                v-for="col in Object.keys(
-                                                    attrMappingToRatio
-                                                ).filter((key) => batchInfoType[key] != null)"
+                                                v-for="col in Object.keys(attrMappingToRatio).filter((key) => batchInfoType[key] != null)"
                                                 :prop="attrMappingToRatio[col]"
                                                 :label="batchInfoType[col]"
                                                 width="60"
                                             ></el-table-column>
-                                            <el-table-column
-                                                prop="totalQuantityRatio"
-                                                label="对/件"
-                                                width="240"
-                                            />
+                                            <el-table-column prop="totalQuantityRatio" label="对/件" width="240" />
                                             <el-table-column prop="unitPerRatio" label="件数" />
                                         </el-table>
                                     </template>
                                 </el-table-column>
+                                <el-table-column prop="shoeTypeColorName" label="中文颜色" width="90" />
+                                <el-table-column prop="customerColorName" label="英文颜色" width="90" />
                                 <el-table-column
-                                    prop="shoeTypeColorName"
-                                    label="中文颜色"
-                                    width="90"
-                                />
-                                <el-table-column
-                                    prop="customerColorName"
-                                    label="英文颜色"
-                                    width="90"
-                                />
-                                <el-table-column
-                                    v-for="col in Object.keys(attrMappingToAmount).filter(
-                                        (key) => batchInfoType[key] != null
-                                    )"
+                                    v-for="col in Object.keys(attrMappingToAmount).filter((key) => batchInfoType[key] != null)"
                                     :prop="`shoeTypeBatchData.${attrMappingToAmount[col]}`"
                                     :label="batchInfoType[col]"
                                     width="60"
                                 ></el-table-column>
-                                <el-table-column
-                                    prop="shoeTypeBatchData.totalAmount"
-                                    label="总数量"
-                                    width="120"
-                                />
+                                <el-table-column prop="shoeTypeBatchData.totalAmount" label="总数量" width="120" />
                                 <el-table-column label="金额" width="120">
                                     <template #default="scope">
                                         <el-input
@@ -138,17 +101,10 @@
                                         </el-input>
                                     </template>
                                 </el-table-column>
-                                <el-table-column
-                                    prop="shoeTypeBatchData.totalPrice"
-                                    label="总金额"
-                                />
+                                <el-table-column prop="shoeTypeBatchData.totalPrice" label="总金额" />
                                 <el-table-column label="鞋型">
                                     <template #default="scope">
-                                        <el-image
-                                            :src="imagerUrl(scope.row.shoeTypeImgUrl)"
-                                            style="width: 150px; height: 100px"
-                                            v-if="scope.row.shoeTypeImgUrl"
-                                        ></el-image>
+                                        <el-image :src="imagerUrl(scope.row.shoeTypeImgUrl)" style="width: 150px; height: 100px" v-if="scope.row.shoeTypeImgUrl"></el-image>
                                         <span v-else>暂无图片数据</span>
                                     </template>
                                 </el-table-column>
@@ -160,27 +116,10 @@
                     <el-table-column prop="currentStatus" label="鞋型状态" />
                     <el-table-column label="备注">
                         <template #default="scope">
-                            <el-button
-                                v-if="!scope.row.orderShoeRemarkExist"
-                                type="primary"
-                                size="default"
-                                @click="openRemarkDialog(scope.row)"
-                                style="margin-left: 20px"
-                                >添加备注
-                            </el-button>
+                            <el-button v-if="!scope.row.orderShoeRemarkExist" type="primary" size="default" @click="openRemarkDialog(scope.row)" style="margin-left: 20px">添加备注 </el-button>
 
-                            <el-text
-                                v-if="scope.row.orderShoeRemarkExist"
-                                style="display: inline-block"
-                                >{{ scope.row.orderShoeRemarkRep }}</el-text
-                            >
-                            <el-button
-                                v-if="scope.row.orderShoeRemarkExist"
-                                type="warning"
-                                size="default"
-                                @click="openEditRemarkDialog(scope.row)"
-                                style="margin-left: 20px; margin-top: -20px"
-                            >
+                            <el-text v-if="scope.row.orderShoeRemarkExist" style="display: inline-block">{{ scope.row.orderShoeRemarkRep }}</el-text>
+                            <el-button v-if="scope.row.orderShoeRemarkExist" type="warning" size="default" @click="openEditRemarkDialog(scope.row)" style="margin-left: 20px; margin-top: -20px">
                                 编辑备注
                             </el-button>
                         </template>
@@ -188,19 +127,9 @@
                 </el-table>
 
                 <span>
-                    <el-button
-                        type="primary"
-                        @click="saveFormData"
-                        v-if="orderData.orderStatus === 7 || role == 1"
-                        >保存数据</el-button
-                    >
+                    <el-button type="primary" @click="saveFormData" v-if="orderData.orderStatus === 7 || role == 1">保存数据</el-button>
                     <!--<el-button type="warning" @click="rejectOrder" v-if="orderData.orderStatus === 7">订单退回</el-button> -->
-                    <el-button
-                        type="primary"
-                        @click="showMessage"
-                        v-if="orderData.orderStatus === 7"
-                        >完成审批</el-button
-                    >
+                    <el-button type="primary" @click="showMessage" v-if="orderData.orderStatus === 7">完成审批</el-button>
                 </span>
             </el-main>
         </el-container>
@@ -208,21 +137,11 @@
     <el-dialog title="鞋型备注" v-model="remarkDialogVis" width="50%">
         <el-form>
             <el-form-item label="工艺备注">
-                <el-input
-                    type="textarea"
-                    :rows="2"
-                    v-model="remarkForm.technicalRemark"
-                    :maxlength="255"
-                ></el-input>
+                <el-input type="textarea" :rows="2" v-model="remarkForm.technicalRemark" :maxlength="255"></el-input>
             </el-form-item>
 
             <el-form-item label="材料备注">
-                <el-input
-                    type="textarea"
-                    :rows="2"
-                    v-model="remarkForm.materialRemark"
-                    :maxlength="255"
-                ></el-input>
+                <el-input type="textarea" :rows="2" v-model="remarkForm.materialRemark" :maxlength="255"></el-input>
             </el-form-item>
         </el-form>
 
@@ -234,30 +153,15 @@
             </span>
         </template>
     </el-dialog>
-    <el-dialog
-        title="退回流程"
-        v-model="isRevertDialogVisable"
-        width="20%"
-        :close-on-click-modal="false"
-    >
+    <el-dialog title="退回流程" v-model="isRevertDialogVisable" width="20%" :close-on-click-modal="false">
         <span>
             <span>退回流程</span>
             <el-row :gutter="20">
                 <el-col :span="24" :offset="0">
                     <el-form>
                         <el-form-item label="退回至状态" prop="revertToStatus">
-                            <el-select
-                                v-model="revertForm.revertToStatus"
-                                placeholder="请选择退回至状态"
-                                clearable
-                                @change="handleStatusSelect"
-                            >
-                                <el-option
-                                    v-for="item in revertStatusReasonOptions"
-                                    :key="item.status"
-                                    :label="item.statusName"
-                                    :value="item.status"
-                                ></el-option>
+                            <el-select v-model="revertForm.revertToStatus" placeholder="请选择退回至状态" clearable @change="handleStatusSelect">
+                                <el-option v-for="item in revertStatusReasonOptions" :key="item.status" :label="item.statusName" :value="item.status"></el-option>
                             </el-select>
                         </el-form-item>
                         <el-form-item label="需要中间流程" prop="isNeedMiddleProcess">
@@ -267,20 +171,10 @@
                             </el-radio-group>
                         </el-form-item>
                         <el-form-item label="退回原因" prop="revertReason">
-                            <el-input
-                                v-model="revertForm.revertReason"
-                                :rows="4"
-                                placeholder="请输入退回原因"
-                                disabled
-                            ></el-input>
+                            <el-input v-model="revertForm.revertReason" :rows="4" placeholder="请输入退回原因" disabled></el-input>
                         </el-form-item>
                         <el-form-item label="退回详细原因" prop="revertDetail">
-                            <el-input
-                                v-model="revertForm.revertDetail"
-                                type="textarea"
-                                :rows="4"
-                                placeholder="请输入退回原因"
-                            ></el-input>
+                            <el-input v-model="revertForm.revertDetail" type="textarea" :rows="4" placeholder="请输入退回原因"></el-input>
                         </el-form-item>
                     </el-form>
                 </el-col>
@@ -289,7 +183,7 @@
         <template #footer>
             <span>
                 <el-button @click="isRevertDialogVisable = false">取消</el-button>
-                <el-button type="primary" :disabled="revertForm.revertToStatus === ''"  @click="saveRevertForm">确认</el-button>
+                <el-button type="primary" :disabled="revertForm.revertToStatus === ''" @click="saveRevertForm">确认</el-button>
             </span>
         </template>
     </el-dialog>
@@ -363,19 +257,15 @@ onMounted(() => {
 })
 
 async function getOrderInfo() {
-    const response = await axios.get(
-        `${$api_baseUrl}/order/getbusinessorderinfo?orderid=${orderId}`
-    )
+    const response = await axios.get(`${$api_baseUrl}/order/getbusinessorderinfo?orderid=${orderId}`)
     console.log(orderData)
     orderData.value = response.data
     orderShoeData.value = response.data.orderShoeAllData
     batchInfoType = response.data.batchInfoType
     orderData.value.orderShoeAllData.forEach((orderShoe) =>
         orderShoe.orderShoeTypes.forEach((orderShoeType) => {
-            orderShoeTypeIdToUnitPrice[orderShoeType.orderShoeTypeId] =
-                orderShoeType.shoeTypeBatchData.unitPrice
-            orderShoeTypeIdToCurrencyType[orderShoeType.orderShoeTypeId] =
-                orderShoeType.shoeTypeBatchData.currencyType
+            orderShoeTypeIdToUnitPrice[orderShoeType.orderShoeTypeId] = orderShoeType.shoeTypeBatchData.unitPrice
+            orderShoeTypeIdToCurrencyType[orderShoeType.orderShoeTypeId] = orderShoeType.shoeTypeBatchData.currencyType
         })
     )
 }
@@ -402,6 +292,12 @@ async function submitFormData() {
     if (response.status === 200) {
         ElMessage.success('审批成功')
         getOrderInfo()
+        if (window.opener?.callRefreshTaskData) {
+            window.opener.callRefreshTaskData()
+            console.log('Function called successfully.')
+        } else {
+            console.warn('Function not available on opener window.')
+        }
     } else {
         ElMessage.error('审批失败')
     }
@@ -453,9 +349,7 @@ async function submitRemarkForm() {
     }
 }
 function handleStatusSelect(value) {
-    revertForm.revertReason = revertStatusReasonOptions.value.find(
-        (item) => item.status === value
-    ).reason
+    revertForm.revertReason = revertStatusReasonOptions.value.find((item) => item.status === value).reason
 }
 function openRevertDialog() {
     isRevertDialogVisable.value = true
@@ -478,15 +372,11 @@ async function getAllRevertStatusReasonOptions() {
     }
 }
 function saveRevertForm() {
-    ElMessageBox.confirm(
-        `确定退回此订单吗？退回至 ${revertForm.revertToStatus}, 原因是 ${revertForm.revertReason}`,
-        '提示',
-        {
-            confirmButtonText: '确定',
-            cancelButtonText: '取消',
-            type: 'warning'
-        }
-    )
+    ElMessageBox.confirm(`确定退回此订单吗？退回至 ${revertForm.revertToStatus}, 原因是 ${revertForm.revertReason}`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+    })
         .then(() => {
             revertOrder()
         })
@@ -507,6 +397,12 @@ async function revertOrder() {
         ElMessage.success('退回成功')
         getOrderInfo()
         isRevertDialogVisable.value = false
+        if (window.opener?.callRefreshTaskData) {
+            window.opener.callRefreshTaskData()
+            console.log('Function called successfully.')
+        } else {
+            console.warn('Function not available on opener window.')
+        }
     } else {
         ElMessage.error('退回失败')
     }
