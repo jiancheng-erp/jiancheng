fb4164e1be8e3b9fa94c7bc019787b6e4d02e880
ericzhong <ericzhong@baninetech.com>
2025-09-17 14:44:07 +0800

    修改供应商管理页面，
    1. 添加修改/删除供应商
    2. 添加搜索
    3. 开发部和物控部用一个供应商页面

--- backend-python/logistics/supplier_page.py
+++ backend-python/logistics/supplier_page.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, jsonify, request
+from sqlalchemy import delete
 
 from app_config import db
 from models import *
 
+from logger import logger
+
 supplier_page_bp = Blueprint("supplier_page_bp", __name__)
+
 
 @supplier_page_bp.route("/logistics/allsuppliers", methods=["GET"])
 def get_all_suppliers():
@@ -19,30 +23,68 @@ def get_all_suppliers():
             {
                 "supplierId": supplier.supplier_id,
                 "supplierName": supplier.supplier_name,
+                "supplierType": supplier.supplier_type,
                 "supplierField": supplier_type,
             }
         )
     return jsonify(result)
 
+
 @supplier_page_bp.route("/logistics/createsupplier", methods=["POST"])
 def create_supplier():
     supplier_name = request.json.get("supplierName")
-    supplier_type = request.json.get("supplierField")
+    supplier_type = request.json.get("supplierType")
     if supplier_name is None or supplier_name == "":
         return jsonify({"message": "供应商名称不能为空"}), 400
+    # search whether the supplier exists
+    supplier = Supplier.query.filter_by(supplier_name=supplier_name).first()
+    if supplier:
+        return jsonify({"message": "供应商已存在"}), 400
     supplier = Supplier(supplier_name=supplier_name, supplier_type=supplier_type)
     db.session.add(supplier)
     db.session.commit()
     return jsonify({"message": "success"})
 
-@supplier_page_bp.route("/logistics/addcompositesupplier", methods=["POST"])
-def add_composite_supplier():
+
+@supplier_page_bp.route("/logistics/editsupplier", methods=["PUT"])
+def edit_supplier():
+    logger.debug(f"/logistics/editsupplier data: {request.json}")
+    supplier_id = request.json.get("supplierId")
     supplier_name = request.json.get("supplierName")
-    # search whether the supplier exists
-    supplier = Supplier.query.filter_by(supplier_name=supplier_name).first()
-    if supplier:
-        return jsonify({"message": "供应商已存在"}), 400
-    supplier = Supplier(supplier_name=supplier_name, supplier_type="W")
-    db.session.add(supplier)
+    supplier_type = request.json.get("supplierType")
+    if supplier_name is None or supplier_name == "":
+        return jsonify({"message": "供应商名称不能为空"}), 400
+
+    # check whether the supplier exists
+    existed_supplier = (
+        db.session.query(Supplier)
+        .filter(Supplier.supplier_name == supplier_name)
+        .first()
+    )
+    if existed_supplier and existed_supplier.supplier_id != supplier_id:
+        return jsonify({"message": "该供应商名称已存在"}), 400
+
+    supplier = db.session.query(Supplier).filter_by(supplier_id=supplier_id).first()
+    if not supplier:
+        return jsonify({"message": "供应商不存在"}), 404
+    supplier.supplier_name = supplier_name
+    supplier.supplier_type = supplier_type
+    db.session.commit()
+    return jsonify({"message": "success"})
+
+
+@supplier_page_bp.route("/logistics/deleteSupplier", methods=["DELETE"])
+def delete_supplier():
+    supplier_id = request.json.get("supplierId")
+    if supplier_id is None or supplier_id < 1:
+        return jsonify({"message": "无效供应商id"}), 400
+
+    # check whether the supplier referenced
+    material = db.session.query(Material).filter(Material.material_supplier == supplier_id).first()
+    if material:
+        return jsonify({"message": "该供应商已被物料引用，无法删除"}), 400
+    
+    stmt = delete(Supplier).where(Supplier.supplier_id == supplier_id)
+    db.session.execute(stmt)
     db.session.commit()
     return jsonify({"message": "success"})
--- frontend/jiancheng/src/Pages/LogisticsControlDepartment/LogisticsControlManager/components/SupplierManagementView.vue
+++ frontend/jiancheng/src/Pages/LogisticsControlDepartment/LogisticsControlManager/components/SupplierManagementView.vue
@@ -2,27 +2,35 @@
     <el-row :gutter="20">
         <el-col :span="24" style="font-size: xx-large; text-align: center;">供应商管理</el-col>
     </el-row>
-    
+
+    <el-row :gutter="20">
+        <el-col>
+            <el-input v-model="searchQuery" placeholder="搜索供应商名称" clearable @clear="filterSuppliers"
+                @change="filterSuppliers" style="width: 300px;" />
+        </el-col>
+    </el-row>
+
     <el-row :gutter="20" style="margin-top: 20px;">
         <el-col :span="24">
             <el-table :data="paginatedSupplierData" border style="height: 500px;">
                 <el-table-column prop="supplierName" label="供应商名称"></el-table-column>
                 <el-table-column prop="supplierField" label="供应商供货类型"></el-table-column>
+                <el-table-column label="操作">
+                    <template #default="scope">
+                        <el-button type="primary" @click="openEditDialog(scope.row)">修改</el-button>
+                        <el-button type="danger" @click="deleteSupplier(scope.row)">删除</el-button>
+                    </template>
+                </el-table-column>
             </el-table>
         </el-col>
     </el-row>
-    
+
     <!-- Pagination -->
     <el-row :gutter="20" style="margin-top: 20px;">
         <el-col :span="24" style="text-align: center;">
-            <el-pagination
-                @size-change="handleSizeChange"
-                @current-change="handleCurrentChange"
-                :current-page="currentPage"
-                :page-size="pageSize"
-                :page-sizes="[5, 10, 20, 50]"
-                layout="total, sizes, prev, pager, next, jumper"
-                :total="supplierData.length">
+            <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                :current-page="currentPage" :page-size="pageSize" :page-sizes="[5, 10, 20, 50]"
+                layout="total, sizes, prev, pager, next, jumper" :total="supplierData.length">
             </el-pagination>
         </el-col>
     </el-row>
@@ -39,7 +47,7 @@
                 <el-input v-model="addSupplierData.supplierName"></el-input>
             </el-form-item>
             <el-form-item label="供应商供货类型: ">
-                <el-select v-model="addSupplierData.supplierField" placeholder="请选择供货类型">
+                <el-select v-model="addSupplierData.supplierType" placeholder="请选择供货类型">
                     <el-option v-for="item in FieldData" :key="item.value" :label="item.label" :value="item.value">
                     </el-option>
                 </el-select>
@@ -49,6 +57,26 @@
             <span>
                 <el-button @click="cancelCreateSupplier">取消</el-button>
                 <el-button type="primary" @click="confirmSubmit">确认</el-button>
+            </span>
+        </template>
+    </el-dialog>
+
+    <el-dialog title="修改供应商" v-model="isEditSupplierDialogVisible" width="30%">
+        <el-form>
+            <el-form-item label="供应商名称: ">
+                <el-input v-model="addSupplierData.supplierName"></el-input>
+            </el-form-item>
+            <el-form-item label="供应商供货类型: ">
+                <el-select v-model="addSupplierData.supplierType" placeholder="请选择供货类型">
+                    <el-option v-for="item in FieldData" :key="item.value" :label="item.label" :value="item.value">
+                    </el-option>
+                </el-select>
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <span>
+                <el-button @click="closeEditSupplierDialog">取消</el-button>
+                <el-button type="primary" @click="editSupplier">确认</el-button>
             </span>
         </template>
     </el-dialog>
@@ -67,13 +95,17 @@ export default {
             currentPage: 1, // Current page
             pageSize: 10, // Items per page
             addSupplierData: {
+                supplierId: null,
                 supplierName: '',
+                supplierType: '',
                 supplierField: ''
             },
             FieldData: [
                 { value: 'N', label: '普通供货商' },
                 { value: 'W', label: '外加工供货商' }
-            ]
+            ],
+            searchQuery: '', // Search query
+            isEditSupplierDialogVisible: false
         }
     },
     computed: {
@@ -88,6 +120,62 @@ export default {
         this.getSupplierData();
     },
     methods: {
+        openEditDialog(row) {
+            this.addSupplierData = JSON.parse(JSON.stringify(row));
+            this.isEditSupplierDialogVisible = true;
+        },
+        async editSupplier() {
+            try {
+                let data = {
+                    supplierId: this.addSupplierData.supplierId,
+                    supplierName: this.addSupplierData.supplierName,
+                    supplierType: this.addSupplierData.supplierType
+                }
+                await axios.put(`${this.$apiBaseUrl}/logistics/editsupplier`, data);
+            }
+            catch (error) {
+                console.error("Error editing supplier:", error);
+                if (error.response) {
+                    ElMessage.error(error.response.data.message);
+                }
+                else {
+                    ElMessage.error('修改供应商失败');
+                }
+                return;
+            }
+            this.closeEditSupplierDialog();
+            this.getSupplierData();
+            ElMessage.success('修改成功');
+        },
+        filterSuppliers() {
+            if (!this.searchQuery) {
+                this.getSupplierData();
+                return;
+            }
+            const query = this.searchQuery.toLowerCase();
+            this.supplierData = this.supplierData.filter(supplier =>
+                supplier.supplierName.toLowerCase().includes(query)
+            );
+            this.currentPage = 1; // Reset to first page
+        },
+        async deleteSupplier(row) {
+            const supplierId = row.supplierId;
+            try {
+                await axios.delete(`${this.$apiBaseUrl}/logistics/deleteSupplier`, {
+                    data: { supplierId: supplierId }
+                });
+                this.getSupplierData();
+                ElMessage.success('删除成功');
+            } catch (error) {
+                console.error("Error deleting supplier:", error);
+                if (error.response) {
+                    ElMessage.error(error.response.data.message);
+                }
+                else {
+                    ElMessage.error('删除供应商失败');
+                }
+            }
+        },
         async getSupplierData() {
             try {
                 const response = await axios.get(`${this.$apiBaseUrl}/logistics/allsuppliers`);
@@ -115,10 +203,18 @@ export default {
                 }
             }
         },
-        cancelCreateSupplier() {
+        emptySupplierData() {
+            this.addSupplierData.supplierId = null;
             this.addSupplierData.supplierName = '';
             this.addSupplierData.supplierField = '';
+        },
+        cancelCreateSupplier() {
+            this.emptySupplierData();
             this.isCreateSupplierDialogVisible = false;
+        },
+        closeEditSupplierDialog() {
+            this.emptySupplierData();
+            this.isEditSupplierDialogVisible = false;
         },
         confirmSubmit() {
             ElMessageBox.confirm('确认提交吗?', '提示', {
