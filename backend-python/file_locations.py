41a9e865183af63e6223fcdb9ef234577f5b4409
evannwang <augwangzongda@gmail.com>
2025-11-02 22:36:57 +0800

    deploy changes

--- backend-python/file_locations.py
+++ backend-python/file_locations.py
@@ -1,3 +1,3 @@
-FILE_STORAGE_PATH = "/home/desktop/jc-dev/file_server"
+FILE_STORAGE_PATH = "/home/admin/erp-demo/erp-files"
 IMAGE_STORAGE_PATH = "http://localhost:12667/"
-IMAGE_UPLOAD_PATH = "/home/desktop/jc-dev/img_server"
+IMAGE_UPLOAD_PATH = "/home/admin/erp-demo/img-server"
--- backend-python/file_locations_shz.py
+++
@@ -1,3 +0,0 @@
-FILE_STORAGE_PATH = "D:/temp"
-IMAGE_STORAGE_PATH = "http://localhost:12667/"
-IMAGE_UPLOAD_PATH = "D:/imgtmp"
